8b2c21cf23c6279e3b38be95da80615491e831b8
Ori Mizrahi <ori.mizrahi1000@gmail.com>
2025-11-20 15:38:02 +0200

    Basically game

--- src/tetris.py
+++ src/tetris.py
@@ -1,35 +1,82 @@
 import numpy as np
+import random
 
 class Tetris:
     def __init__(self):
         self._init_game()
-        self.pieces = np.array([[[1,1,1,1]], # line
+        self.pieces = [[[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]], # line
                                 [[1,1], [1,1]], # square
-                                [[0,1], [1,1], [0,1]], # t shaped
-                                [[0,1], [1,1], [1,0]], # S shaped
-                                [[1,0], [1,1], [0,1]], # Reversed S
-                                [[1,0], [1,0], [1,1]], # L 
-                                [[0,1], [0,1], [1,1]], # Reversed L
-                                ])
-        self.cur_piece = np.random.choice(self.pieces)
-        self.next_piece = np.random.choice(self.pieces)
+                                [[0,1,0], [1,1,0], [0,1,0]], # t shaped
+                                [[0,0,1], [0,1,1], [0,1,0]], # S shaped
+                                [[1,0,0], [1,1,0], [0,1,0]], # Reversed S
+                                [[0,1,0], [0,1,0], [0,1,1]], # L 
+                                [[0,1,0], [0,1,0], [1,1,0]], # Reversed L
+                                ]
+
+        self.x = 4
+        self.y = 0
+        self.cur_piece_index = random.randint(0, len(self.pieces)-1)
+        self.cur_piece = np.array(self.pieces[self.cur_piece_index])
+        self.next_piece_index = random.randint(0, len(self.pieces)-1)
+        self.next_piece = np.array(self.pieces[self.next_piece_index])
+        self._insert_piece()
 
 
     def _init_game(self):
-        self.board = np.array([[0 for _ in range(10)] for _ in range(24)])
+        self.board = np.array([[0 for _ in range(10)] for _ in range(20)])
 
-    def insert_piece(self, x, y, piece):
-        pass
+    def _insert_piece(self):
+        for row in range(len(self.cur_piece)):
+            for col in range(len(self.cur_piece[0])):
+                if(self.board[self.y+row][self.x+col] == 1 and self.cur_piece[row][col] == 1):
+                    return
 
-    def rotate_piece(self, x, y, piece):
-        pass
+        for row in range(len(self.cur_piece)):
+            for col in range(len(self.cur_piece[0])):
+                self.board[self.y+row][self.x+col] ^= self.cur_piece[row][col]
 
-    def move_piece(self, x, y, piece):
-        pass
+    def _remove_piece(self):
+        for row in range(len(self.cur_piece)):
+            for col in range(len(self.cur_piece[0])):
+                self.board[self.y+row][self.x+col] = 0
+    
+    def rotate_piece(self):
+        if self.cur_piece_index == 1: # if square don't do anything
+            return 
+
+        self._remove_piece()
+        self.cur_piece = np.rot90(self.cur_piece, -1)
+        self._insert_piece()
+
+    def move_down_piece(self):
+        # if hit floor
+        if self.y + len(self.cur_piece) == len(self.board):
+            self.new_next_piece()
+            self._insert_piece()
+            return
+
+        self._remove_piece()
+        self.y += 1
+        self._insert_piece()
+
+    def move_right_piece(self):
+        self._remove_piece()
+        self.x += 1
+        self._insert_piece()
+
+    def move_left_piece(self):
+        self._remove_piece()
+        self.x -= 1
+        self._insert_piece()
 
     def new_next_piece(self):
-        self.cur_piece = self.next_piece
-        self.next_piece = np.random.choice(self.pieces)
+        self.x = 4
+        self.y = 0
+
+        self.cur_piece_index = self.next_piece_index
+        self.cur_piece = np.array(self.pieces[self.cur_piece_index])
+        self.next_piece_index = random.randint(0, len(self.pieces)-1)
+        self.next_piece = np.array(self.pieces[self.next_piece_index])
 
     def agent_random_move(self):
         pass
---
+++ src/window.py
@@ -0,0 +1,72 @@
+from tetris import Tetris
+import pygame
+
+class Window:
+    def __init__(self):
+        self.RES = (1000,1000)
+        self.game_agent = Tetris()
+        self.AGENT = (50, 450) # where the board starts and ends
+
+        self.game_player = Tetris()
+        self.PLAYER = (550, 950) # where the board starts and ends
+
+        self.BLOCK_SIZE = 40 # 40x40 pixels
+
+        self.HEIGHT = 100 # we'll start at y=100 up to y=900
+
+
+        pygame.init()
+        pygame.font.init()
+        self.font = pygame.font.SysFont("Arial", 30)
+        self.screen = pygame.display.set_mode(self.RES)
+        self.clock = pygame.time.Clock()
+
+    def draw_game(self):
+        for row in range(len(self.game_agent.board)):
+            for col in range(len(self.game_agent.board[0])):
+                # Agent Board
+                if(self.game_agent.board[row][col] == 0):
+                    pygame.draw.rect(self.screen, (0,0,0), (self.AGENT[0] + self.BLOCK_SIZE*col, self.HEIGHT + self.BLOCK_SIZE*row, self.BLOCK_SIZE, self.BLOCK_SIZE), 1)
+                else:
+                    pygame.draw.rect(self.screen, (255,0,0), (self.AGENT[0] + self.BLOCK_SIZE*col, self.HEIGHT + self.BLOCK_SIZE*row, self.BLOCK_SIZE, self.BLOCK_SIZE), 0)
+
+                # Player board
+                if(self.game_player.board[row][col] == 0):
+                    pygame.draw.rect(self.screen, (0,0,0), (self.PLAYER[0] + self.BLOCK_SIZE*col, self.HEIGHT + self.BLOCK_SIZE*row, self.BLOCK_SIZE, self.BLOCK_SIZE), 1)
+                else:
+                    pygame.draw.rect(self.screen, (255,0,0), (self.PLAYER[0] + self.BLOCK_SIZE*col, self.HEIGHT + self.BLOCK_SIZE*row, self.BLOCK_SIZE, self.BLOCK_SIZE), 0)
+    
+    def draw_text(self):
+        agent_text = self.font.render("Agent", True, (0,0,0))
+        player_text = self.font.render("Player", True, (0,0,0))
+        self.screen.blit(agent_text, ((self.AGENT[1] - self.AGENT[0]) / 2, 50))
+        self.screen.blit(player_text, ((self.PLAYER[1] - self.PLAYER[0]) / 2 + 500, 50))
+                
+    def start(self):
+        running = True
+        MOVEEVENT, t = pygame.USEREVENT+1, 1000 
+        pygame.time.set_timer(MOVEEVENT, t)
+        while running:
+            self.screen.fill("white")
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        self.game_player.rotate_piece()
+                    if event.key == pygame.K_a:
+                        self.game_player.move_left_piece()
+                    if event.key == pygame.K_d:
+                        self.game_player.move_right_piece()
+
+                if event.type == MOVEEVENT:
+                    self.game_player.move_down_piece()
+
+            self.draw_game()
+            self.draw_text()
+            pygame.display.flip()
+            self.clock.tick(60)
+
+        pygame.quit()
+
